9bd216a47195c68e550cf516a171f079b7880b24
Leonardorossato <leonardorossato21@gmail.com>
2023-06-09 10:20:48 -0300

    update main.ts swagger api ok

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Swiggy Backend Nestjs Api')
+    .setDescription(
+      'Swiggy Backend Api with NestJs, TypeOrm, Swagger, Postgres and Docker',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.APP_PORT);
 }
-bootstrap();
+void bootstrap();
