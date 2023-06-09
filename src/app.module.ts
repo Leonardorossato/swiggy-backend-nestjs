ef46c90386d03917d2bdf703eeae7c00026ae847
Leonardorossato <leonardorossato21@gmail.com>
2023-06-09 16:31:36 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresSqlConnection } from './config/ormconfig';
+import { AuthModule } from './auth/auth.module';
+import { KeycloakModule } from './keycloak/keycloak.module';
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(PostgresSqlConnection),
+    AuthModule,
+    KeycloakModule,
   ],
 })
 export class AppModule {}
