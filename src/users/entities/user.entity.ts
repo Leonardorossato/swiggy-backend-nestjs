7a09d6faca27b3f49447039205066dc35df7a21f
Leonardorossato <leonardorossato21@gmail.com>
2023-06-10 09:28:28 -0300

    update user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+}
