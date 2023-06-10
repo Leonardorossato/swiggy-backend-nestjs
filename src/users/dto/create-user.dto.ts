f63399e8d18b7bd9916b3f115660dbb31f2f72e9
Leonardorossato <leonardorossato21@gmail.com>
2023-06-10 09:28:36 -0300

    update create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+}
