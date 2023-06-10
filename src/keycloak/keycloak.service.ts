15f30d388d549a90580d9dd325c4e244c768cd79
Leonardorossato <leonardorossato21@gmail.com>
2023-06-10 08:47:03 -0300

    update keycloak.service.ts create keycloak configuration

--- src/keycloak/keycloak.service.ts
+++ src/keycloak/keycloak.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import {
+  KeycloakConnectOptions,
+  KeycloakConnectOptionsFactory,
+  PolicyEnforcementMode,
+  TokenValidation,
+} from 'nest-keycloak-connect';
 
 @Injectable()
-export class KeycloakService {}
+export class KeycloakConfigService implements KeycloakConnectOptionsFactory {
+  createKeycloakConnectOptions(): KeycloakConnectOptions {
+    return {
+      authServerUrl: process.env.KEYCLOAK_URL,
+      realm: 'master',
+      clientId: process.env.KEYCLOAK_CLIENT_ID,
+      secret: process.env.KEYCLOAK_SECRET,
+      policyEnforcement: PolicyEnforcementMode.PERMISSIVE,
+      tokenValidation: TokenValidation.ONLINE,
+      'bearer-only': true,
+    };
+  }
+}
