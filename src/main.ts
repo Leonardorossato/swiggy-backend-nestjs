ac8fc2356173bd21a7297947da69e9f997194af1
Leonardorossato <leonardorossato21@gmail.com>
2023-06-12 10:28:06 -0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,15 @@ async function bootstrap() {
     .addBearerAuth()
     .setVersion('1.0')
     .build();
-
+  const memoryStore = new session.MemoryStore();
+  app.use(
+    session({
+      secret: process.env.KEYCLOAK_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      store: memoryStore,
+    }),
+  );
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
